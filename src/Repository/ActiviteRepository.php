b32a04d86fff04431c43ec38da4bf8f91f9a0551
iheb <iheb.hmidi@esprit.tn>
2026-04-07 00:07:49 +0100

    integration user + hebergement + trajets + activites

--- src/Repository/ActiviteRepository.php
+++ src/Repository/ActiviteRepository.php
@@ -14,4 +14,38 @@ class ActiviteRepository extends ServiceEntityRepository
     }
 
     // Add custom methods as needed
+
+    /**
+     * @return Activite[] Returns an array of Activite objects
+     */
+    public function findDisponibles(): array
+    {
+        // Par défaut, s'il n'y a pas de colonne 'statut', on retourne toutes les activités
+        return $this->createQueryBuilder('a')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * @return Activite[] Returns an array of Activite objects
+     */
+    public function findByFilters(?string $type, ?float $prixMax): array
+    {
+        $qb = $this->createQueryBuilder('a');
+
+        if ($prixMax !== null) {
+            $qb->andWhere('a.prixParPersonne <= :prixMax')
+               ->setParameter('prixMax', $prixMax);
+        }
+        
+        // Si vous ajoutez un champ 'type' plus tard sur l'entité Activite
+        /*
+        if ($type) {
+            $qb->andWhere('a.type = :type')
+               ->setParameter('type', $type);
+        }
+        */
+
+        return $qb->getQuery()->getResult();
+    }
 }
