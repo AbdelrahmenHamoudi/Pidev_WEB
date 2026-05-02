e977dcafc3b7aa251094577291027c9b9caea1c0
AyetteBoubaya <boubayaayette@isima.u-monastir.tn>
2026-05-02 15:46:34 +0100

    Save current local state of activite branch

---
+++ tests/ActiviteManagerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ActiviteManagerTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
